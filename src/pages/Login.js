a73e29f4f491df53f9c4d2a1dc10884afdc55b42
Matheus Reis <matheus.reisluz@gmail.com>
2021-03-20 13:20:21 -0300

    pagina de login criada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,34 @@
-import logo from '../assets/images/logoLab.png';
-import '../assets/css/App.css';
+import splashArt from '../assets/images/splash.png';
+import '../assets/css/reset.css';
+import '../assets/css/login.css';
 
 function Login() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao LabTrip para agências - Login.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/LabTrip"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LabTrip no github.
-        </a>
-      </header>
+    <div className="container-principal">
+
+      <div className="container-duplo">
+
+        <div className="imagem-principal">
+          <img src={splashArt} alt="Boas vindas Labtrip">
+          </img>
+        </div>
+
+        <div className="formulario-principal">
+          <h1>Olá! Seja bem vindo ao LapTrip para agências.</h1>
+          <form>
+          <input type="text" placeholder="seuemail@gmail.com"/> 
+          <input type="text" placeholder="Senha"/> 
+          <input type="submit" value="Entrar"/>
+          </form>
+          <a href="#">
+            Esqueceu sua senha?
+          </a>
+          <a href="#">
+            Primeiro acesso?
+          </a>
+        </div>
+
+      </div>
     </div>
   );
 }
